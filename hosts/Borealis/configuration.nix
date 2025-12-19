01c35f7e50ecb043ebb8fcb8b01d891a9c057f5c
blob1234567890 <ben.k.olson@gmail.com>
2025-12-19 11:53:08 +0900

    I caved: migrated applications from home-manager to homebrew here. it's way better

--- hosts/Borealis/configuration.nix
+++ hosts/Borealis/configuration.nix
@@ -1,5 +1,6 @@
 { pkgs, ... }: {
     users.users.ben.home = "/Users/ben";
+    system.primaryUser = "ben";
 
     environment = {
         shells = with pkgs; [ zsh bash ];
@@ -13,10 +14,51 @@
 
     security.pam.services.sudo_local.touchIdAuth = true;
 
-    imports = [ ];
+    # networking.firewall.allowedTCPPorts = [
+    #     57621 # allow syncing with local mobile devices
+    #     5353  # allow discovery of Spotify Connect (e.g. Google Cast) devices
+    # ];
+
+    homebrew = {
+        enable = true;
+        onActivation.autoUpdate = true;
+        onActivation.cleanup = "zap";
+        # onActivation.upgrade = true;
+        # caskArgs.no_quarantine = true;
+
+        casks = [
+            "1password"
+            "anki"
+            "discord"
+            "element"
+            "firefox"
+            "google-chrome"
+            "librewolf"
+            "obsidian"
+            # TODO: "olympus"
+            # TODO: "qbittorrent"
+            "signal"
+            "spotify"
+            "steam"
+            "tor-browser"
+            "vlc"
+            # TODO: "WEPA-PrintApp"
+            "zoom" # TODO: isolate
+            "zotero"
+        ];
+
+        masApps = {
+            "Brother iPrint&Scan" = 1193539993;
+            "Notability: Smarter AI Notes" = 360593530;
+            Xcode = 497799835;
+        };
+    };
+
+    imports = [
+    ];
 
     ##### Technical Details #####
-    # Bachwards compatibility
+    # Backwards compatibility
     system.stateVersion = 6;
     nix.settings.experimental-features = [
         "nix-command"
