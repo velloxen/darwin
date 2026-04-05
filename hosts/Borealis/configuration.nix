2f36fc756662addc1cde3f699de372d3542b5bdd
blob1234567890 <ben.k.olson@gmail.com>
2026-04-05 15:16:06 +0900

    skim

--- hosts/Borealis/configuration.nix
+++ hosts/Borealis/configuration.nix
@@ -1,74 +1,79 @@
-{ pkgs, ... }: {
-    users.users.ben.home = "/Users/ben";
-    system.primaryUser = "ben";
+{ pkgs, ... }:
+{
+  users.users.ben.home = "/Users/ben";
+  system.primaryUser = "ben";
 
-    environment = {
-        shells = with pkgs; [ zsh bash ];
-        systemPackages = [
-            pkgs.coreutils
-            pkgs.mkvtoolnix
-        ];
-        systemPath = [ "/usr/local/bin" ];
-        pathsToLink = [ "/Applications" ]; # symlinks /run/current-system/sw
-    };
+  environment = {
+    shells = with pkgs; [
+      zsh
+      bash
+    ];
+    systemPackages = [
+      pkgs.coreutils
+      pkgs.mkvtoolnix
+    ];
+    systemPath = [ "/usr/local/bin" ];
+    pathsToLink = [ "/Applications" ]; # symlinks /run/current-system/sw
+  };
 
-    security.pam.services.sudo_local.touchIdAuth = true;
+  security.pam.services.sudo_local.touchIdAuth = true;
 
-    # networking.firewall.allowedTCPPorts = [
-    #     57621 # allow syncing with local mobile devices
-    #     5353  # allow discovery of Spotify Connect (e.g. Google Cast) devices
-    # ];
+  # networking.firewall.allowedTCPPorts = [
+  #     57621 # allow syncing with local mobile devices
+  #     5353  # allow discovery of Spotify Connect (e.g. Google Cast) devices
+  # ];
 
-    homebrew = {
-        enable = true;
-        enableZshIntegration = true;
-        onActivation.autoUpdate = true;
-        onActivation.cleanup = "zap";
-        # onActivation.upgrade = true;
-        # caskArgs.no_quarantine = true;
+  homebrew = {
+    enable = true;
+    enableZshIntegration = true;
+    onActivation.autoUpdate = true;
+    onActivation.cleanup = "zap";
+    # onActivation.upgrade = true;
+    # caskArgs.no_quarantine = true;
 
-        casks = [
-            "1password"
-            "anki"
-            "discord"
-            "element"
-            "firefox"
-            #"gimp"
-            "google-chrome"
-            "inkscape" # NOTE: see https://github.com/flameshot-org/flameshot/issues/4125 about Gatekeeper
-            #"librewolf"
-            "obsidian"
-            # TODO: "olympus"
-            # TODO: "qbittorrent"
-            "signal"
-            "spotify"
-            "steam"
-            "tor-browser"
-            "vlc"
-            # TODO: "WEPA-PrintApp"
-            "zoom" # TODO: isolate
-            "zotero"
+    casks = [
+      "1password"
+      "anki"
+      "discord"
+      "element"
+      "firefox"
+      #"gimp"
+      "google-chrome"
+      "inkscape" # NOTE: see https://github.com/flameshot-org/flameshot/issues/4125 about Gatekeeper
+      #"librewolf"
+      "obsidian"
+      # TODO: "olympus"
+      # TODO: "qbittorrent"
+      "signal"
+      "skim"
+      "spotify"
+      "steam"
+      "tor-browser"
+      "vlc"
+      # TODO: "WEPA-PrintApp"
+      "zoom" # TODO: isolate
+      "zotero"
 
-            "minecraft"
-            "curseforge"
-        ];
-
-        masApps = {
-            "Brother iPrint&Scan" = 1193539993;
-            "Notability: Smarter AI Notes" = 360593530;
-            Xcode = 497799835;
-        };
-    };
-
-    imports = [
+      "minecraft"
+      "curseforge"
     ];
 
-    ##### Technical Details #####
-    # Backwards compatibility
-    system.stateVersion = 6;
-    nix.settings.experimental-features = [
-        "nix-command"
-        "flakes"
-        # "pipe-operators"
-    ];
+    masApps = {
+      "Brother iPrint&Scan" = 1193539993;
+      "Notability: Smarter AI Notes" = 360593530;
+      Xcode = 497799835;
+    };
+  };
+
+  imports = [
+  ];
+
+  ##### Technical Details #####
+  # Backwards compatibility
+  system.stateVersion = 6;
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+    # "pipe-operators"
+  ];
 }
