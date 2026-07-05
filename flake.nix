0a7824d7ae0d489f85bbc5da55fa2b4d7d5a4e49
blob1234567890 <ben.k.olson@gmail.com>
2026-07-05 19:14:26 +0900

    upgraded darwin

--- flake.nix
+++ flake.nix
@@ -1,30 +1,46 @@
 {
-    description = "Borealis nix-darwin system flake";
+  description = "Borealis nix-darwin system flake";
 
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
-        darwin.url = "github:nix-darwin/nix-darwin/master";
-        darwin.inputs.nixpkgs.follows = "nixpkgs";
+  inputs = {
+    nixpkgs = {
+      owner = "NixOS";
+      repo = "nixpkgs";
+      #ref = "nixpkgs-unstable";
+      rev = "cbb5cf358f50aa6acc9efd6113b7bcfbc352cd73";
+      type = "github";
     };
+    #.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-    outputs = {
-        self,
-        nixpkgs,
-        darwin,
-        ...
-    } @ inputs: {
-        # Build darwin flake using:
-        # $ darwin-rebuild build --flake .#Borealis
-        darwinConfigurations."Borealis" = darwin.lib.darwinSystem {
-            system = "aarch64-darwin";
-            pkgs = import nixpkgs {
-                system = "aarch64-darwin";
-                config.allowUnfree = true;
-            };
-            modules = [
-                ./hosts/Borealis/configuration.nix
-            ];
+    #darwin.url = "github:nix-darwin/nix-darwin/master";
+    darwin = {
+      owner = "nix-darwin";
+      repo = "nix-darwin";
+      ref = "master";
+      #rev = "6a771120d607dcccb279a27d227650e324815c35";
+      type = "github";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      darwin,
+      ...
+    }@inputs:
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#Borealis
+      darwinConfigurations."Borealis" = darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        pkgs = import nixpkgs {
+          system = "aarch64-darwin";
+          config.allowUnfree = true;
         };
+        modules = [
+          ./hosts/Borealis/configuration.nix
+        ];
+      };
     };
 }
