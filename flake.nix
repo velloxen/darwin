0d9351ba7cfa1c995e6e2402e9ee3616cac5a9e5
blob1234567890 <ben.k.olson@gmail.com>
2026-07-07 11:20:59 +0900

    they fixed the issue

--- flake.nix
+++ flake.nix
@@ -2,20 +2,10 @@
   description = "Borealis nix-darwin system flake";
 
   inputs = {
-    # NOTE: This nixpkgs commit (https://github.com/NixOS/nixpkgs/commit/ad97f5573d6621d4861bfdcfbd433ca08e86a188) made a breaking change, this nix-darwin issue (https://github.com/nix-darwin/nix-darwin/issues/1817) is resolving it. This nix-darwin PR (https://github.com/nix-darwin/nix-darwin/pull/1818) is the fix.
-    # The easy solution is to just wait a day or two.
-    nixpkgs.url = "github:NixOS/nixpkgs/832530da7df4f84f5234a0e37388fe918c725ede";
-    #.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nipkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-    #darwin.url = "github:nix-darwin/nix-darwin/master";
-    darwin = {
-      owner = "nix-darwin";
-      repo = "nix-darwin";
-      ref = "master";
-      #rev = "6a771120d607dcccb279a27d227650e324815c35";
-      type = "github";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    darwin.url = "github:nix-darwin/nix-darwin/master";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
